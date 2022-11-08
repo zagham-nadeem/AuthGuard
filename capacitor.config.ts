14c2c59bfb266bfb79b66e52768c5eb0b056bfb8
Zagham Nadeem <Zagham@learn2earnn.com>
2022-11-09 01:08:39 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zagham.auth',
+  appName: 'AuthGuard',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
